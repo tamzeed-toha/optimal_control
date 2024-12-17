a1f1451b2718700dd77a787e609be8bd8d42b2af
tamzeed <tamzeed90@gmail.com>
2024-12-16 18:24:38 -0800

    preliminary draft done

--- project/functions.py
+++ project/functions.py
@@ -65,13 +65,31 @@ class SystemAgent2D:
         x_dot = [x2_dot, x1_dot, y2_dot, y1_dot, theta3_dot, theta2_dot, theta1_dot, phi2_dot, phi1_dot]
         self.x_dot = sym.Matrix(x_dot)
         
+        self.A, self.B = self.linearize()
         
     def f(self, state, u):
         subs = {self.x2: state[0], self.x1: state[1], self.y2: state[2], self.y1: state[3], self.theta3: state[4], self.theta2: state[5], self.theta1: state[6], self.phi2: state[7], self.phi1: state[8], self.F: u[0], self.u_freq: u[1], self.tau: u[2]} 
         return np.array(self.x_dot.subs(subs)).astype(np.float64).reshape(-1)
 
 
-    # def 
+    def linearize(self):
+        A = self.x_dot.jacobian(self.states).subs({self.F: 0, self.u_freq: 0, self.tau: 0})
+        B = self.x_dot.jacobian(self.u)
+        # small angle approximation
+        B = B.subs({sym.sin(self.phi1): self.phi1, sym.cos(self.phi1): 1})
+        return A, B
+    
+    def linearized_f(self, state, u):
+        if isinstance(state, list):
+            state = np.array(state).astype(np.float64).reshape(-1)
+        if isinstance(u, list):
+            u = np.array(u).astype(np.float64).reshape(-1)
+        state_sub = {self.x2: state[0], self.x1: state[1], self.y2: state[2], self.y1: state[3], self.theta3: state[4], self.theta2: state[5], self.theta1: state[6], self.phi2: state[7], self.phi1: state[8]}
+        A = np.array(self.A.subs(state_sub)).astype(np.float64)
+        B = np.array(self.B.subs(state_sub)).astype(np.float64)
+        value = A @ state + B @ u
+        return np.array(value).astype(np.float64).reshape(-1)
+        
 
 
 
